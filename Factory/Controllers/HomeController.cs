8c759da489ca529ee590510657996c139f392a5a
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-03-23 09:59:18 -0700

    add controller for home page with index action that will list both machines and engineers

---
+++ Factory/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly FactoryContext _db;
+    public HomeController(FactoryContext db)
+    {
+      _db = db; 
+    }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      Engineer[] engineerArray = _db.Engineers.ToArray();
+      Machine[] machineArray = _db.Machines.ToArray();
+      Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+      model.Add("engineers",engineerArray);
+      model.Add("machines", machineArray);
+      return View(model);
+    }
+  }
+}
